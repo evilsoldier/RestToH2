92cffc067923c4a35c879e26ecae6b647e62b1e1
georgi.trendafilov <georgi.trendafilov@scalefocus.com>
2017-04-26 10:08:42 +0300

    enable init() method in Application.class

--- src/main/java/com/rest/Application.java
+++ src/main/java/com/rest/Application.java
@@ -39,20 +39,20 @@ import io.hawt.web.AuthenticationFilter;
 public class Application extends SpringBootServletInitializer {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
-	
+
 	@Autowired
 	private ServletContext servletContext;
 
 	HashSet<Efc> efcs = new HashSet<>();
 
 	public static void main(String[] args) {
-		System.setProperty("hawtio.authenticationEnabled", "false");
 		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
 		return application.sources(Application.class);
 	}
 
@@ -88,8 +88,8 @@ public class Application extends SpringBootServletInitializer {
 			logger.info("The sample data has been generated");
 		};
 	}
-	
-//	@Bean
+
+	// @Bean
 	public EmbeddedServletContainerFactory servletContainer() {
 		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
 			@Override
@@ -107,53 +107,59 @@ public class Application extends SpringBootServletInitializer {
 		return tomcat;
 	}
 
-	 private Connector initiateHttpConnector() {
-		    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		    connector.setScheme("http");
-		    connector.setPort(8080);
-		    connector.setSecure(false);
-		   connector.setRedirectPort(8888);
-		    
-		    return connector;
-		  }
-	// @PostConstruct
-		public void init() {
-			final ConfigManager configManager = new ConfigManager();
-			configManager.init();
-			servletContext.setAttribute("ConfigManager", configManager);
-		}
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8888);
 
-		/**
-		 * Loading an example plugin
-		 * @return
-		 */
-		//@Bean
-		public HawtPlugin samplePlugin() {
-			return new HawtPlugin("sample-plugin", "/hawtio/plugins", "", new String[] { "sample-plugin/js/sample-plugin.js" });
-		}
-		
-		/**
-		 * Set things up to be in offline mode
-		 * @return
-		 * @throws Exception
-		 */
-		//@Bean
-		public ConfigFacade configFacade() throws Exception {
-			ConfigFacade config = new ConfigFacade() {
-				public boolean isOffline() {
-					return true;
-				}
-			};
-			config.init();
-			return config;
-		}
-		
-		/**
-		 * Register rest endpoint to handle requests for /plugin, and return all registered plugins.
-		 * @return
-		 */
-		//@Bean
-		public PluginService pluginService(){
-			return new PluginService();
-		}
+		return connector;
+	}
+
+	@PostConstruct
+	public void init() {
+		final ConfigManager configManager = new ConfigManager();
+		configManager.init();
+		servletContext.setAttribute("ConfigManager", configManager);
+	}
+
+	/**
+	 * Loading an example plugin
+	 * 
+	 * @return
+	 */
+	// @Bean
+	public HawtPlugin samplePlugin() {
+		return new HawtPlugin("sample-plugin", "/hawtio/plugins", "",
+				new String[] { "sample-plugin/js/sample-plugin.js" });
+	}
+
+	/**
+	 * Set things up to be in offline mode
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	// @Bean
+	public ConfigFacade configFacade() throws Exception {
+		ConfigFacade config = new ConfigFacade() {
+			public boolean isOffline() {
+				return true;
+			}
+		};
+		config.init();
+		return config;
+	}
+
+	/**
+	 * Register rest endpoint to handle requests for /plugin, and return all
+	 * registered plugins.
+	 * 
+	 * @return
+	 */
+	// @Bean
+	public PluginService pluginService() {
+		return new PluginService();
+	}
 }
