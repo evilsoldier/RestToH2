4bfdfaa03a0b11a60081dca0bd27fab6e92fa799
georgi.trendafilov <georgi.trendafilov@scalefocus.com>
2017-04-27 07:41:46 +0300

    rf controller anotations

--- src/main/java/com/rest/controller/HtmlController.java
+++ src/main/java/com/rest/controller/HtmlController.java
@@ -1,13 +1,14 @@
 package com.rest.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class HtmlController {
 
 	@RequestMapping("/greeting")
