bd9653e54df0aeb2c3758be97eaf87b0b51f1f14
georgi.trendafilov <georgi.trendafilov@scalefocus.com>
2017-05-12 13:56:58 +0300

    moving to Thymeleaf

--- src/main/java/com/rest/controller/HtmlController.java
+++ src/main/java/com/rest/controller/HtmlController.java
@@ -1,20 +1,33 @@
 package com.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
-@RequestMapping("/")
+import com.rest.repository.PersonService;
+
+@Controller
 public class HtmlController {
 
-	@GetMapping("/")
-	public String mainController() {
-		return "Hello, seems like your application is running :)";
+	@Autowired
+	PersonService personService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		return "home";
 	}
 
-	@RequestMapping(value = "/ping", method = RequestMethod.GET)
+	@GetMapping("/person")
+	public String welcome(Model model, @RequestParam(value = "id", defaultValue = "1") Long id) {
+		model.addAttribute("person", personService.findById(id));
+		return "person";
+	}
+
+	@RequestMapping(value = "/ping", method = RequestMethod.GET, produces = "application/json")
 	public String ping() {
 		return "pong";
 	}
