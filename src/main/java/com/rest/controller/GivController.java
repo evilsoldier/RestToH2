7ec0c3fd9195d3f49420674923c8a6837a5ae7e8
Georgi Trendafilov <georgi.trendafilov@scalefocus.com>
2019-06-14 16:41:57 +0300

    Add counter

--- src/main/java/com/rest/controller/GivController.java
+++ src/main/java/com/rest/controller/GivController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,22 @@ public class GivController {
 
     private static final Logger log = LoggerFactory.getLogger(GivController.class);
 
+    private long syncCount = 0L;
+
     @PostMapping(value = "/KOHLS_GIV_DSVProcessAvailableInventorySnapShot_Sync", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity postGiv(@RequestBody String sync) throws InterruptedException {
-        log.info("Received: {}", sync);
+        log.info("Received: {}", ++syncCount, sync);
 
         // Sleep 100ms to simulate networking delay
+
         Thread.sleep(100L);
+        return ResponseEntity.ok().body("OK");
+    }
+
+    @GetMapping(value = "/reset", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity resetCount() {
+        log.info("Reset counter");
+        syncCount = 0L;
         return ResponseEntity.ok().body("OK");
     }
 
