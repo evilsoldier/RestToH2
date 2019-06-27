c13506918254c8afda66a0bd6ae83cc0ad22acde
Georgi Trendafilov <georgi.trendafilov@scalefocus.com>
2019-06-27 13:02:47 +0300

    Add OmsController

---
+++ src/main/java/com/rest/controller/OmsController.java
@@ -0,0 +1,29 @@
+package com.rest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Georgi Trendafilov
+ */
+@RestController
+@RequestMapping("/efc/test/")
+public class OmsController {
+
+    private static final Logger log = LoggerFactory.getLogger(OmsController.class);
+
+    @PostMapping(value = "/invoice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity postInvoice(HttpEntity data) {
+
+        log.info("Received: {},", data.getBody());
+
+        return ResponseEntity.ok().body("OK");
+    }
+
+}
